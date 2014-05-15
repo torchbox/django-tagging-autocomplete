40c0e3fadf4e1d05d8139058efd923833401fac5
Neal Todd <neal.todd@torchbox.com>
2014-05-15 11:59:05 +0100

    Django 1.7 compatibility.
    Replacing deprecated simplejson with json

--- tagging_autocomplete/tests.py
+++ tagging_autocomplete/tests.py
@@ -1,6 +1,7 @@
+import json
+
 from django.core.urlresolvers import reverse
 from django.test import TestCase
-from django.utils import simplejson
 
 class TaggingAutocompleteViewTest(TestCase):
     urls = 'tagging_autocomplete.urls'
@@ -10,5 +11,5 @@ class TaggingAutocompleteViewTest(TestCase):
         test view list_tags without submitting a term
         """
         response = self.client.get(reverse('tagging_autocomplete-list'))
-        data = simplejson.loads(response.content)
+        data = json.loads(response.content)
         self.assertEqual(data, [])
